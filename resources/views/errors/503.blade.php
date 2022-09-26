03b359d29961ef055f83f1af56777d16fdf0c4fe
desplega <desplega@desplega.com>
2022-09-26 19:45:18 +0200

    Error pages 404 and 503

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -1,1 +1,11 @@
-503
+@extends('layouts.master', ['page' => ''])
+
+@section('titulo', 'Moto no encontrada')
+
+@section('contenido')
+    <figure class="row mt-2 mb-2 col-5 offet-1">
+        <img class="d-block w-100" alt="Moto" src="{{ asset('images/errors/503.png') }}">
+    </figure>
+
+    <p class="fs-1 text-center">503</p>
+@endsection
