d23e13bdeaa91bb5c7a92283b7f8c38565e2ef78
desplega <desplega@desplega.com>
2022-09-29 15:56:49 +0200

    Review maintenance page

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -1,10 +1,13 @@
 @extends('layouts.master')
 
-@section('titulo', 'Error del servidor')
+@section('titulo', 'Modo de mantenimiento')
 
 @section('contenido')
+    <br><br>
+    <p class="fs-1">En mantenimiento...</p>
+
     <figure class="text-center">
-        <img class="rounded mx-auto d-block" alt="Error de servidor" src="{{ asset('images/errors/503.png') }}">
+        <img class="rounded mx-auto d-block" alt="Modo de mantenimiento" src="{{ asset('images/errors/503.png') }}">
     </figure>
 
     <p class="fs-1 text-center">503</p>
