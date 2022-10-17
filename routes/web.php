2fba1286a720dd7d8ff8bc3b069d784e07bd4e15
desplega <desplega@desplega.com>
2022-10-17 21:30:34 +0200

    Remove Intelephense warning

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\WelcomeController;
 use App\Http\Controllers\ContactoController;
 use App\Http\Controllers\HomeController;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth; // Not required, added to avoid warnings by Intelephense
 
 /*
 |--------------------------------------------------------------------------
