3bea87f135a45e3324977da302e206870d15afc7
desplega <desplega@desplega.com>
2022-10-20 19:43:51 +0200

    API for Bike CRUD

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Models\Bike;
+use App\Http\Controllers\BikeApiController;
 
 /*
 |--------------------------------------------------------------------------
@@ -23,4 +24,18 @@ Route::get('/bikes', function() {
     // $json = json_encode(Bike::orderBy('id', 'DESC')->get());
     // return response($json)->header('Content-Type', 'application/json');
     return response(Bike::orderBy('id', 'DESC')->get());
+});
+
+Route::get('/motos', [BikeApiController::class, 'index']);
+Route::get('/moto/{bike}', [BikeApiController::class, 'show'])
+    ->where('bike', '^\d+$'); // Only digits
+Route::get('/motos/{campo}/{valor}', [BikeApiController::class, 'search'])
+    ->where('campo', '^marca|modelo|matricula$');
+Route::post('/moto', [BikeApiController::class, 'store']);
+Route::put('/moto/{bike}', [BikeApiController::class, 'update'])
+    ->where('bike', '^\d+$'); // Only digits;
+Route::delete('/moto/{bike}', [BikeApiController::class, 'delete'])
+    ->where('bike', '^\d+$'); // Only digits;
+Route::fallback(function() {
+    return response(['status' => 'BAD REQUEST'], 400);
 });
