b79ddc3b4db2273fbac86b1f898a241a89b78743
desplega <desplega@desplega.com>
2022-10-23 20:23:39 +0200

    Reduce welcome image size

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
     <p>Implementación de un <b>CRUD</b> de motos.</p>
 
     <figure class="mt-2 mb-2 col-10 offet-1">
-        <img class="rounded d-block" alt="Moto" src="{{ asset('images/bikes/moto.jpg') }}">
+        <img width="400" class="rounded d-block" alt="Moto" src="{{ asset('images/bikes/moto.jpg') }}">
     </figure>
 @endsection
 
